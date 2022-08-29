3bcb54df427d21b874d967d3b2b9d2af50c2eac2
daniel.bistox <daniel@bistox.com>
2022-08-29 18:24:27 -0400

    fix: app syntax for imported components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect } from "react";
-import Table from "./components/Table";
+import { Table } from "./components";
 
 export default function App() {
   const [size] = useState(41);
